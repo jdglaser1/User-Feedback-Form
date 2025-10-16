d1a3d9ca1b96a459039174f159426a8472b48ece
Joshua Glaser <jdglaser@usf.edu>
2025-10-16 16:53:19 -0400

    started tooltip with mouseover/mouseout

--- cp_1.js
+++ cp_1.js
@@ -1,21 +1,27 @@
+// Starting by identifying all of the elements we will use from the html file
+const userText = document.getElementById("user-name")
+const emailText = document.getElementById("email")
+const commentText = document.getElementById("comments")
+const countCharacters = document.getElementById("count-characters")
+
+
 //Step 3 creating event listeners
 
- const countCharacters = document.getElementById('userName')
- Button.addEventListener('click',() =>{
-     console.log(countCharacters.length)
+//counting characters
+commentText.addEventListener("input", function() {
+    countCharacters.textContent = commentText.value.length
+})
+
+ //mouseover tooltip(user section)
+ userText.addEventListener("mouseover", function(){ // we  defined the user section as userText above
+  document.getElementById("user-name-tip").style.display ="block"
  })
+userText.addEventListener("mouseout", function(){
+    document.getElementById("user-name-tip").style.display="none"
+})
 
 
- //mouseover
- const card = document.querySelector("main.emailArea");
-
-card.addEventListener("mouseover", () => {
-  card.style.backgroundColor = "#eef6e6";
-});
-
-card.addEventListener("mouseout", () => {
-  card.style.backgroundColor = "white";
-});
+ //creating a feedback display
 
 
 // Be sure to add event.preventDefault()
